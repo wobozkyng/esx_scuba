e233522276a65373e08e7846625258293bc0e19e
wobozkyng <42249669+wobozkyng@users.noreply.github.com>
2023-01-02 00:29:32 +0700

    Upload ox_inventory reference files

---
+++ ox_inventory/data/items.lua
@@ -0,0 +1,18 @@
+return {
+    ['scuba_set'] = {
+		label = 'Scuba Set',
+		weight = 2000,
+		description = 'Diving equipment, longer underwater',
+		client = {
+			export = 'esx_scuba.wear'
+		}
+	},
+    ['scuba_fins'] = {
+		label = 'Scuba Fins',
+		weight = 200,
+		description = 'Diving equipment, swimming assitance',
+		client = {
+			export = 'esx_scuba.wear'
+		}
+	}
+}
