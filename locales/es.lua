5e2e45d624d87d5af0d24370afbba70f84710724
AL1ƎN <72218928+johnsoul777@users.noreply.github.com>
2023-01-02 22:56:52 +0100

    Cresate es.lua
    Translation in Spanish

---
+++ locales/es.lua
@@ -0,0 +1,11 @@
+Locales['es'] = {
+    ['no_tank'] = 'Actualmente no tiene el tanque de oxigeno',
+    ['tank_loaded'] = 'Tanque cargado %s %s',
+    ['tank_capacity'] = 'La capacidad actual del tanque es %s %s',
+    ['tank_remaining'] = 'La capacidad restante del tanque es %s %s',
+    ['tank_available'] = 'Tanque de oxígeno disponible con %s %s capacidad',
+    ['tank_not_available'] = 'Tanque de oxígeno no disponible',
+    ['diving_fins'] = 'Buceo con aletas de natación',
+    ['diving_no_fins'] = 'Bucear sin aletas de natación',
+    ['push_refill'] = 'Pulsa [E] para rellenar el tanque de oxígeno',
+}
